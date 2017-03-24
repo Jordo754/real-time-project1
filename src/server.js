0d4f3ce211da1813e7b3394c90814913c2e3ada5
Jordan <jordo754@gmail.com>
2017-03-24 14:54:10 -0400

    almost there

--- src/server.js
+++ src/server.js
@@ -18,20 +18,54 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 
 // pass in the http server into socketio and grab the websocket server as io
 const io = socketio(app);
-
-// object for rooms and users
-const roomList = {};
-
-
+let i = 0;
+let userCount = 0;
+const baseBoard = {
+  colors: ['white', 'black', 'white', 'black', 'white', 'black', 'white', 'black',
+           'black', 'white', 'black', 'white', 'black', 'white', 'black', 'white',
+           'white', 'black', 'white', 'black', 'white', 'black', 'white', 'black',
+           'black', 'white', 'black', 'white', 'black', 'white', 'black', 'white',
+           'white', 'black', 'white', 'black', 'white', 'black', 'white', 'black',
+           'black', 'white', 'black', 'white', 'black', 'white', 'black', 'white',
+           'white', 'black', 'white', 'black', 'white', 'black', 'white', 'black',
+           'black', 'white', 'black', 'white', 'black', 'white', 'black', 'white'],
+  spaces: [0, 1, 0, 1, 0, 1, 0, 1,
+           1, 0, 1, 0, 1, 0, 1, 0,
+           0, 1, 0, 1, 0, 1, 0, 1,
+           0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0,
+           2, 0, 2, 0, 2, 0, 2, 0,
+           0, 2, 0, 2, 0, 2, 0, 2,
+           2, 0, 2, 0, 2, 0, 2, 0],
+};
 
 const onJoined = (sock) => {
   const socket = sock;
 
   socket.on('join', (data) => {
-    if (roomList['room1'].keys.size() < 4) {
-      roomList['room1'][roomList.keys.size()] = socket;
-    } else {
-      socket.emit('join', {code: 1});
+    // check if room isn't full
+    if (userCount < 2) {
+      socket.join(`room${i}`);
+      socket.room = `room${i}`;
+      console.log('user joined ' + socket.room);
+      userCount++;
+    }
+
+    // check if room just filled
+    if (userCount >= 2) {
+      // send out the room info to the users
+      const colorRand = Math.round(Math.random() + 1);
+      if (colorRand === 1) {
+        socket.emit('load', { board: baseBoard, color: 1, myTurn: true });
+        socket.broadcast.to(socket.room).emit('load', { board: baseBoard, color: 2, myTurn: false });
+      } else {
+        socket.emit('load', { board: baseBoard, color: 2, myTurn: false });
+        socket.broadcast.to(socket.room).emit('load', { board: baseBoard, color: 1, myTurn: true });
+      }
+
+      // reset count and next room
+      userCount = 0;
+      i++;
     }
   });
 };
@@ -39,6 +73,17 @@ const onJoined = (sock) => {
 const onMsg = (sock) => {
   const socket = sock;
   
+  socket.on('clientUpdate', (data) => {
+    data.boardSpaces[data.currentSpace] = data.boardSpaces[data.selectedPiece];
+    data.boardSpaces[data.selectedPiece] = 0;
+    if (data.jumpedSpace != -1) {
+      data.boardSpaces[data.jumpedSpace] = 0;
+      socket.broadcast.to(socket.room).emit('update', { pieceCount: data.pieceCount - 1, boardSpaces: data.boardSpaces });
+    } else {
+      socket.broadcast.to(socket.room).emit('update', { pieceCount: data.pieceCount, boardSpaces: data.boardSpaces });
+    }
+    
+  });
 };
 
 const onDisconnect = (sock) => {
